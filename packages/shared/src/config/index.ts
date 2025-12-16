38a628cdaf0dac5e0de0d2bde245627cbfc91c20
signature18632 <intmax18@gmail.com>
2025-12-16 11:58:47 +0700

    bridge event watcher bridge requested (#140)
    * bridge event watcher bridge requested
    
    * bridge monitor

--- packages/shared/src/config/index.ts
+++ packages/shared/src/config/index.ts
@@ -80,6 +80,10 @@ export const config = cleanEnv(process.env, {
   MAINNET_BRIDGE_O_APP_CONTRACT_DEPLOYED_BLOCK: num({ devDefault: 0 }),
   BASE_BRIDGE_O_APP_CONTRACT_ADDRESS: str({ devDefault: "0x" }),
   BASE_BRIDGE_O_APP_CONTRACT_DEPLOYED_BLOCK: num({ devDefault: 0 }),
+  // SenderBridgeOApp
+  SENDER_BRIDGE_O_APP_CHAIN_TYPE: str({ devDefault: "l2" }),
+  SENDER_BRIDGE_O_APP_CONTRACT_ADDRESS: str({ devDefault: "0x" }),
+  SENDER_BRIDGE_O_APP_CONTRACT_DEPLOYED_BLOCK: num({ devDefault: 0 }),
   // messenger contract
   L1_SCROLL_MESSENGER_CONTRACT_ADDRESS: str({ devDefault: "0x" }),
   // mock messenger contract (for test and dev)
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { configDefaults, defineConfig } from "vitest/config";
 import path from "path";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
