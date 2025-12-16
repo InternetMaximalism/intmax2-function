02b061f0de3c8b830b8393b75553febc7a6aa59c
signature18632 <intmax18@gmail.com>
2025-12-16 09:44:11 +0700

    update modules (#139)
    * update modules
    
    * fix test for shared

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 import { configDefaults, defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, "**/dist/**"],
+  },
+  resolve: {
+    alias: {
+      "@intmax2-function/shared": path.resolve(process.cwd(), "./packages/shared/src"),
+    },
   },
 });
